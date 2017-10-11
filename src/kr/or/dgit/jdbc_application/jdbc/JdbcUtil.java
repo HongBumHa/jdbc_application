84c3b09a5407db0556f04c141e2e9656061ebbb5
Bum <goldstate@naver.com>
2017-10-11 14:05:57 +0900

    jdbc_application 초기화 작업 및 DB연결 테스트 완료

---
+++ src/kr/or/dgit/jdbc_application/jdbc/JdbcUtil.java
@@ -0,0 +1,59 @@
+package kr.or.dgit.jdbc_application.jdbc;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcUtil {
+	public static void close(Connection con){
+		if(con != null){
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("1");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(PreparedStatement pstmt) {
+		if(pstmt != null){
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("2");
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	public static void close(ResultSet rs) {
+		if(rs != null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("3");
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	public static void close(OutputStreamWriter dos) {
+		if(dos != null){
+			try {
+				dos.close();
+			} catch (IOException e) {
+				System.out.println("4");
+				e.printStackTrace();
+			}
+	
+		}
+	}
+}
